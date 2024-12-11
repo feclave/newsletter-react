e5b2fad8ba824adb8911d853331c7d913ccf050b
Felipe <felipeclave@gmail.com>
2024-12-11 17:29:46 -0300

    criação do formulário com uma condição para renderizar os artigos do blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react"
+
 import ArticleList from "./components/ArticleList"
 import Header from "./components/Header"
+import Form from "./components/Form"
 
 const App = () => {
+  const [user, setUser] = useState()
+  const hasUser = Boolean(user)
+  
   return (
     <>
-      <Header />
-      <ArticleList />
+      <Header user={user} />
+      { hasUser && <ArticleList /> }
+      { hasUser || <Form onSubmit={setUser} /> }
     </>
   )
 }
